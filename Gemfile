77d5c63d347420854de0a18dc8508728a4e1ccdc
manhchuan <manhchuanvnck57@gmail.com>
2016-06-01 17:35:11 +0700

    finish user signup

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 gem 'rails',                '4.2.5'
 gem 'bootstrap-sass',       '3.2.0.0'
 gem 'bcrypt',               '3.1.7'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# require 'digest'
 class User < ActiveRecord::Base
+ 
+ before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  validates_each :birthday do |record, attr, value|
+      record.errors.add(attr, 'must be in the past') if value >= Time.now.to_date
+end
 end
