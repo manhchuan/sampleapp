b5d06fd215f9fe2d2b41e975b7e1f7da644af28a
manhchuan <manhchuanvnck57@gmail.com>
2016-06-03 09:16:59 +0700

    apdate_attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,11 @@ class User < ActiveRecord::Base
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
+<<<<<<< HEAD
     update_attributes(:remember_digest, User.digest(remember_token))
+=======
+    update_attribute(:remember_digest, User.digest(remember_token))
+>>>>>>> df165ce... finish chapter8
   end
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
@@ -32,6 +36,10 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   def forget
+<<<<<<< HEAD
     update_attributes(:remember_digest, nil)
+=======
+    update_attribute(:remember_digest, nil)
+>>>>>>> df165ce... finish chapter8
   end
 end
