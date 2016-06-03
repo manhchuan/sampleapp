81fb02b7c99ebfab757736e4acf6730d9eb6afb4
manhchuan <manhchuanvnck57@gmail.com>
2016-06-03 09:16:59 +0700

    apdate_attributes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-	user = User.find_by(email: params[:session][:email].downcase)
-	  if user && user.authenticate(params[:session][:password])
-	  log_in user
-	  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
-      remember user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
 
 	  else
 	flash.now[:danger] = 'Invalid email/password combination'
@@ -15,7 +14,7 @@ class SessionsController < ApplicationController
   end
 end
 	 def destroy
-	 	log_out
+	 	log_out if logged_in?
 	 	redirect_to root_url
   end
 end
