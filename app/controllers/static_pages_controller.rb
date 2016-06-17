cbfe46f6485fe30b362f4683c0fb2e046f0cfae0
manhchuan <manhchuanvnck57@gmail.com>
2016-06-17 10:13:52 +0700

    add user following
    add following user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-  	 @micropost = current_user.microposts.build if logged_in?
+     if logged_in?
+  	 @micropost = current_user.microposts.build 
      @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
@@ -13,3 +14,5 @@ class StaticPagesController < ApplicationController
   def contact
   end
 end
+end
+
